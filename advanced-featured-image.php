b39321f4f6174814decd22d06112e3d2cdee33bf
Michael Novotny <manovotny@gmail.com>
2016-01-28 22:12:42 -0600

    Cleans up `afi_get_attachment_id_from_url` comments.

--- advanced-featured-image.php
+++ advanced-featured-image.php
@@ -180,9 +180,11 @@ function afi_save_thumbnail( $post_id ) {
 add_action( 'save_post', 'afi_save_thumbnail' );
 
 /**
- * Get the ID of the attachment if exists
- * @param  string $attachment_url  
- * @return mix                  
+ * Gets the attachment id of the image, if it exists.
+ *
+ * @param string $attachment_url
+ *
+ * @return bool|null|string|void
  */
 function afi_get_attachment_id_from_url( $attachment_url = '' ) {
  
@@ -190,26 +192,26 @@ function afi_get_attachment_id_from_url( $attachment_url = '' ) {
 
     $attachment_id = false;
  
-    // If there is no url, return.
+    // Exit if there is no url.
     if ( '' == $attachment_url ) {
 
         return;
 
     }
  
-    // Get the upload directory paths
+    // Get the upload directory paths.
     $upload_dir_paths = wp_upload_dir();
  
-    // Make sure the upload path base directory exists in the attachment URL, to verify that we're working with a media library image
+    // Make sure the upload path base directory exists in the attachment url, to verify that we're working with a media library image.
     if ( false !== strpos( $attachment_url, $upload_dir_paths['baseurl'] ) ) {
  
-        // If this is the URL of an auto-generated thumbnail, get the URL of the original image
+        // If this is the url of an auto-generated thumbnail, get the url of the original image.
         $attachment_url = preg_replace( '/-\d+x\d+(?=\.(jpg|jpeg|png|gif)$)/i', '', $attachment_url );
  
-        // Remove the upload path base directory from the attachment URL
+        // Remove the upload path base directory from the attachment url.
         $attachment_url = str_replace( $upload_dir_paths['baseurl'] . '/', '', $attachment_url );
  
-        // Finally, run a custom database query to get the attachment ID from the modified attachment URL
+        // Run a custom database query to get the attachment id from the modified attachment url.
         $attachment_id = $wpdb->get_var( $wpdb->prepare( "SELECT wposts.ID FROM $wpdb->posts wposts, $wpdb->postmeta wpostmeta WHERE wposts.ID = wpostmeta.post_id AND wpostmeta.meta_key = '_wp_attached_file' AND wpostmeta.meta_value = %s AND wposts.post_type = 'attachment'", $attachment_url ) );
  
     }
