64fb576f06540a302faa446499c8d3097ce826a7
Michael Novotny <manovotny@gmail.com>
2016-01-28 22:52:42 -0600

    Cleans up JavaScript.

--- js/admin.js
+++ js/admin.js
@@ -1,82 +1,83 @@
-
-( function( $ ) {
+(function( $ ) {
+    'use strict';
    
-    $( document ).ready(function(){ 
+    $( document ).ready(function(){
 
-		var afi_custom_image_container	= $( '#custom_image_container' );
-		var afi_delete_custom_image		= $( '.delete-custom-img' );
-		var afi_upload_custom_image		= $( '.upload-custom-img' );
-		 
-		function set_uploader( button, field ) {
+        var afi_custom_image_container = $( '#custom_image_container'),
+            afi_delete_custom_image = $( '.delete-custom-img'),
+            afi_upload_custom_image = $( '.upload-custom-img' );
+         
+        function set_uploader( button, field ) {
 
-			var button_to_click = $( button );
-			var field_to_insert = $( field );
-			// make sure both button and field are in the DOM
-			if( button_to_click && field_to_insert ){ 
-				// when button is clicked show thick box
-			
-				button_to_click.on( 'click', function() {
-					tb_show( '', 'media-upload.php?type=image&TB_iframe=true' );
+            var button_to_click = $( button),
+                field_to_insert = $( field );
 
-					// when the thick box is opened set send to editor button
-					set_send( field );
-					 
-				});
-		    }
+            // Make sure both the button and the field exist.
+            if ( button_to_click && field_to_insert ) {
 
-		}
+                // Show the thick box when the button is clicked.
+                button_to_click.on( 'click', function() {
 
-		function set_send( field ) {
-			// store send_to_event so at end of function normal editor works
-			window.original_send_to_editor = window.send_to_editor;
+                    tb_show( '', 'media-upload.php?type=image&TB_iframe=true' );
 
-			// override function so you can have multiple uploaders pre page
-			window.send_to_editor = function( html ) {
+                    // When the thick box is opened, set send to editor button.
+                    set_send( field );
+                     
+                });
+            }
 
-				 
-				var image_url = $( 'img', html ).attr( 'src' );
+        }
 
-				//If imgurl is undefined, then there is only one img from link
-				if( undefined === image_url ) {
+        function set_send( field ) {
 
-					image_url = $( html ).attr( 'src' );
+            // Store `send_to_event`, so at end of the function, the editor works correctly.
+            window.original_send_to_editor = window.send_to_editor;
 
-				}
+            // Override function so you can have multiple uploaders pre page.
+            window.send_to_editor = function( html ) {
 
-		 		$( field ).val( image_url );
-				afi_custom_image_container.append( '<img src="' + image_url + '" style="max-width:100%;" />' );
-				afi_delete_custom_image.removeClass( 'hidden' );
-				afi_upload_custom_image.addClass( 'hidden' );
-				tb_remove();
+                var image_url = $( 'img', html ).attr( 'src' );
 
+                // If image url is undefined, then there is only one image from link.
+                if ( undefined === image_url ) {
 
-				// Set normal uploader for editor
-				window.send_to_editor = window.original_send_to_editor;
-			};
-		}
+                    image_url = $( html ).attr( 'src' );
 
-		function remove_image( button ){
+                }
 
-			$( button ).on( 'click', function( e ) {
-			 	
-			 	e.preventDefault(); 
+                $( field ).val( image_url );
 
-				afi_custom_image_container.empty();
+                afi_custom_image_container.append( '<img src="' + image_url + '" style="max-width: 100%;" />' );
+                afi_delete_custom_image.removeClass( 'hidden' );
+                afi_upload_custom_image.addClass( 'hidden' );
 
-				$( '.afi-img-id' ).val( '' );
-				 
-				afi_delete_custom_image.addClass( 'hidden' );
+                tb_remove();
 
-				afi_upload_custom_image.removeClass( 'hidden' );
+                // Set normal uploader for the editor.
+                window.send_to_editor = window.original_send_to_editor;
+            };
+        }
 
-			});
+        function remove_image( button ) {
 
-		}
+            $( button ).on( 'click', function( event ) {
+                 
+                event.preventDefault();
 
+                afi_custom_image_container.empty();
 
-		set_uploader( '.upload-custom-img', '.afi-img-id' );
-		remove_image( '.delete-custom-img' );
+                $( '.afi-img-id' ).val( '' );
 
-	});
-    
+                afi_delete_custom_image.addClass( 'hidden' );
+                afi_upload_custom_image.removeClass( 'hidden' );
+
+            });
+
+        }
+
+        set_uploader( '.upload-custom-img', '.afi-img-id' );
+        remove_image( '.delete-custom-img' );
+
+    });
+
 })( jQuery );
